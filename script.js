4ef8c5f3570b57f9ee6bd614c847ca686484d2ac
Xjaoneks <104683922+Xjaoneks@users.noreply.github.com>
2025-02-05 13:03:17 +0100

    TraumaHelper - 1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+function ShowWarn() {
+    alert("Není to dodělaný. kdyby něco kontaktuj Xjaoneks na Discordu")
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none"
+    } else {
+      x.style.display = "block";
+    }
+  }
